eec927d318e1ea4a111a04317cffcd8159a8928c
HannaB <phantom@sictamil.com>
2022-05-17 23:01:51 +0300

    Added bot functionality

--- bot.py
+++ bot.py
@@ -2,16 +2,23 @@
 from pyrogram import Client
 from pyrogram.types import Message
 
-from secrets import CLIENT_SECRET
+from secrets import CLIENT_SECRET, STICKER_TO_WORD, HELLO_ID, SONG_ID
 
 app = Client(**CLIENT_SECRET)
+
+WORD_TO_STICKER = {v: k for k, v in STICKER_TO_WORD.items()}
 
 
 @app.on_message()
 def reply_message(_, msg: Message):
-    msg.reply_text("Привіт!")
-    msg.reply_sticker('CAACAgIAAxkBAAOLYoK4i1VTbrB32xLm8556nTA7uB4AAt4cAALyzRlIIYbPY7VRnWUeBA')
-    msg.reply_text("Шукаєте дівчину? Шукайте, успіхів!")
+    if not hasattr(msg, 'sticker') or msg.sticker is None or msg.sticker.file_unique_id not in STICKER_TO_WORD:
+        reply_txt = "Привіт!"
+        msg.reply_text(reply_txt)
+        msg.reply_sticker(HELLO_ID)
+        msg.reply_text("Шукаєте дівчину? Шукайте, успіхів!")
+        msg.reply_audio(SONG_ID)
+    else:
+        msg.reply_text(STICKER_TO_WORD[msg.sticker.file_unique_id])
     return None
 
 
