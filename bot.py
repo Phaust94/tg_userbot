ecbd851ef2924080b22de9f403fd9d6052ae8857
HannaB <phantom@sictamil.com>
2022-09-15 10:52:07 +0300

    Don't reply to self

--- bot.py
+++ bot.py
@@ -2,14 +2,14 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from secrets import CLIENT_SECRET, STICKER_TO_WORD, HELLO_ID, SONG_ID
+from secrets import CLIENT_SECRET, STICKER_TO_WORD, HELLO_ID, SONG_ID, SELF_ID
 
 app = Client(**CLIENT_SECRET)
 
 WORD_TO_STICKER = {v: k for k, v in STICKER_TO_WORD.items()}
 
 
-@app.on_message(filters=(filters.text | filters.sticker) & filters.private)
+@app.on_message(filters=(filters.text | filters.sticker) & filters.private & (~ filters.user(SELF_ID)))
 def reply_message(_, msg: Message):
     if not hasattr(msg, 'sticker') or msg.sticker is None or msg.sticker.file_unique_id not in STICKER_TO_WORD:
         reply_txt = "Привіт!"
